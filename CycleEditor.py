42f5f04fab9072223ecc3d082f84e4e18277a8f3
miarhodesUC <rhodesad@mail.uc.edu>
2025-09-15 13:55:58 -0400

    added stacked widget to customizer

---
+++ CycleEditor.py
@@ -0,0 +1,42 @@
+import numpy
+
+class CoatCycle():
+    def __init__(self, cycle_count, arr_reservoir = [], arr_coat_count = []):
+        if len(arr_reservoir) != len(arr_coat_count):
+            print("ERROR reservoir array and coat count array have unequal size")
+            return 0
+        
+        self.arr_reservoir = arr_reservoir
+        self.arr_coat_count = arr_coat_count
+
+        self.cycle_count = cycle_count
+        self.step_count = len(self.arr_reservoir)
+
+        self.current_cycle = 0
+        self.current_step = 0
+        self.current_coat_count = 0
+        self.current_reservoir = 0
+
+    def addStep(self, reservoir, coat):
+        self.arrReservoir.append(reservoir)
+        self.arrCoatCount.append(coat)
+
+    def generateSaveFile(self):
+        pass
+
+    def executeCycle(self):
+        for cycle_index in self.cycle_count:
+            for step_index in self.step_count:
+                self.current_reservoir = self.arr_reservoir[step_index]
+                self.current_coat_count = self.arr_coat_count[step_index]
+                for coat_index in self.current_coat_count:
+                    print("Cycle #{0} Step #{1} : Coating Reservoir #{2} : Coat #{3} deployed".format(cycle_index, step_index, self.current_reservoir, coat_index))
+                self.incrementStep()
+            self.incrementCycle()
+
+    def incrementStep(self):
+        self.current_step =+ 1
+
+    def incrementCycle(self):
+        self.current_cycle =+ 1
+    
