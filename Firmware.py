8429592986528d08e4efcc0e9b3b4780ccb3bdaa
miarhodesUC <rhodesad@mail.uc.edu>
2026-03-31 12:17:55 -0400

    consolidated files & changing config file setup

--- Firmware.py
+++ Firmware.py
@@ -1,6 +1,7 @@
 from pigpio_shell import pigpio_shell as shell
 import pigpio 
 import csv
+import json
 import time
 from numpy import heaviside as u
 import os
@@ -11,6 +12,7 @@ class HAL(): # Contains basic GPIO commands
     PWM_FREQUENCY_LIST = [10, 20, 40, 50, 80, 100, 160, 200, 250, 320, 400, 500, 800, 1000, 1600, 2000, 4000, 8000]
     def __init__(self, pi=pigpio.pi()):
         self.pi = pi
+
     def checkPin(self, pin, method): # raises errors early
         if type(pin) is not int:
             raise TypeError("checkPin error in {0}, {1} is not an integer".format(method, pin))
@@ -18,14 +20,17 @@ class HAL(): # Contains basic GPIO commands
             raise ValueError("checkPin error in {0}, {1} is greater than 31".format(method, pin))
         if pin < 0:
             raise ValueError("checkPin error in {0}, {1} is negative".format(method, pin))
+        
     def setPinHigh(self, pin:int):
         self.checkPin(pin, "setPinHigh")
         self.pi.write(pin, 1)
         print(f"Pin {pin} HIGH")
+
     def setPinLow(self, pin:int):
         self.checkPin(pin, "setPinLow")
         self.pi.write(pin, 0)
         print(f"Pin {pin} LOW")
+
     def setPWM(self, pin, duty_cycle:int, frequency_index:int):
         self.checkPin(pin, "setPWM")
         if type(frequency_index) is not int:
@@ -39,6 +44,7 @@ class HAL(): # Contains basic GPIO commands
             raise ValueError
         self.pi.set_PWM_frequency(pin, self.PWM_FREQUENCY_LIST[frequency_index])
         self.pi.set_PWM_dutycycle(pin, duty_cycle)
+
     def setDirection(self, direction, pin):
         self.checkPin(pin, "setDirection")
         if direction == 1:
@@ -47,7 +53,8 @@ class HAL(): # Contains basic GPIO commands
             self.setPinLow(pin)
         else:
             raise ValueError("Error in HAL.setDirection: {} is an invalid direction input".format(direction))
-    def selectDEMUX(self, selection_index, pin0, pin1):
+        
+    def selectDEMUX(self, selection_index, pin0, pin1): #signaling for demultiplexer
         if pin0 == pin1:
             raise ValueError("Error in HAL.select DEMUX: pin0 = {0} = pin1 = {1}".format(pin0, pin1))
         self.checkPin(pin0, "selectDEMUX - pin0")
@@ -66,7 +73,9 @@ class HAL(): # Contains basic GPIO commands
                 self.setPinHigh(pin0)
                 self.setPinHigh(pin1)
             case _:
-                raise ValueError("Error in HAL.selectDEMUX: {} is an invalid selection index, check type or make sure it is in range [0, 3]".format(selection_index))
+                raise ValueError("Error in HAL.selectDEMUX: {} is an invalid selection index, " \
+                "check type or make sure it is in range [0, 3]".format(selection_index))
+            
     def moveStepperMotor(self, step_pin, direction_pin, direction, duty_cycle, frequency_index):
         if step_pin == direction_pin:
             raise ValueError("Error in HAL.moveStepperMotor: step_pin = {0} = direction_pin = {1}".format(step_pin, direction_pin))
@@ -74,12 +83,15 @@ class HAL(): # Contains basic GPIO commands
             self.setDirection(direction, direction_pin)
         self.setPWM(step_pin, duty_cycle, frequency_index)
         print(f"Starting motor pin {step_pin} @ frequency {self.PWM_FREQUENCY_LIST[frequency_index]}, dutycycle {duty_cycle}")
+
     def stopStepperMotor(self, step_pin, direction_pin):
         self.setPinLow(step_pin)
         self.setPinLow(direction_pin)
         print(f"Stopping motor pin {step_pin}")
+
     def setAsInput(self, pin):
         self.pi.set_mode(pin, 0)
+
     def setAsOutput(self, pin):
         self.pi.set_mode(pin, 1)
 
@@ -96,20 +108,20 @@ class Solenoid():
     VALVE_FREQUENCY_INDEX = 12
 
     # CONSTANT CONFIGS
-    STEP_MODE_VALUE = 0.5 #value accounts for microsteps
-    DISTANCE_PER_STEP = 0.02
+    STEP_MODE_VALUE = 0.5 #value accounts for stepper motor microsteps
+    DISTANCE_PER_STEP = 0.02 #distance in mm per step of the motor
 
     # PIN CONFIGS
     #TODO Have these configs saved in a JSON file or a txt file
     #INPUTS
-    X_SWITCH_PIN = 23
-    Y_SWITCH_PIN = 24
+    
     EITHER_EDGE = 2
     FALLING_EDGE = 1
     RISING_EDGE = 0
     X_LIMIT_EVENT = 1
     Y_LIMIT_EVENT = 2
     MOVING_LIMIT_EVENT = 3
+
     #LOCOMOTIVE MOTORS
     LOCOMOTIVE_DIRECTION_PIN = 17
     DIRECTION_PIN_X = 20
@@ -118,7 +130,10 @@ class Solenoid():
     LOCOMOTIVE_STEP_PIN_Y = 19
     LOCOMOTIVE_STEP_PIN_T = 5
 
-    #SPRAY MOTORS
+    X_SWITCH_PIN = 23
+    Y_SWITCH_PIN = 24
+
+    #SPRAY MOTOR & VALVES
     PERISTALTIC_STEP_PIN = 13
     RESERVOIR_SELECT_HIGHBIT = 21
     RESERVOIR_SELECT_LOWBIT = 16
@@ -131,32 +146,44 @@ class Solenoid():
         #TODO (maybe): Add position tracking for soft limit checks
         #TODO (alt idea): distance sensors for tracking distance? (mostly directed at any future capstone groups)
     def importConfigs(self, config_file:str):
+        configs = json.load("configs.json")
+        self.DIRECTION_PIN_X = configs['motor-pins']['direction-x']
+        self.DIRECTION_PIN_Y = configs['motor-pins']['direction-y']
+        self.LOCOMOTIVE_STEP_PIN_X = configs['motor-pins']['step-x']
+        self.LOCOMOTIVE_STEP_PIN_Y = configs['motor-pins']['step-y']
+        self.LOCOMOTIVE_STEP_PIN_T = configs['motor-pins']['step-t']
+        self.X_SWITCH_PIN = configs['input-pins']['limit-x']
+        self.Y_SWITCH_PIN = configs['input-pins']['limit-y']
+        self.PERISTALTIC_STEP_PIN = configs['motor-pins']['step-pump']
+        self.RESERVOIR_SELECT_HIGHBIT = configs['valve-pins']['reservoir-hi']
+        self.RESERVOIR_SELECT_LOWBIT = configs['valve-pins']['reservoir-lo']
+        self.AIR_VALVE_PIN = configs['valve-pins']['air']
+        self.PWM_FREQUENCY_INDEX = configs['motor-speed']
         pass
 
     def setReservoirSelect(self, reservoir:int): # tooling for selecting coating solution
         self.hal.selectDEMUX(reservoir, self.RESERVOIR_SELECT_LOWBIT, self.RESERVOIR_SELECT_HIGHBIT)
-    def demoMotor(self, steps, frequency_index, direction, axis:str): # tooling to control motor movements by axis
+
+    def demoMotor(self, steps, frequency_index, direction, axis:str): #CAUTION: FOR TESTING PURPOSES ONLY, NOT STOPPED BY LIMITS
         match axis:
             case 'X':
                 print("Moving X")
                 step_pin = self.LOCOMOTIVE_STEP_PIN_X
                 direction_pin = self.DIRECTION_PIN_X
-                limit_pin = self.X_SWITCH_PIN
             case 'Y':
                 print("Moving Y")
                 step_pin = self.LOCOMOTIVE_STEP_PIN_Y
                 direction_pin = self.DIRECTION_PIN_Y
-                limit_pin = self.Y_SWITCH_PIN
             case 'T':
                 direction_pin = self.LOCOMOTIVE_DIRECTION_PIN
                 step_pin = self.LOCOMOTIVE_STEP_PIN_T
             case _:
-                raise ValueError("Error in moveMotor: {} is an invalid axis".format(axis))
+                raise ValueError("Error in demoMotor: {} is an invalid axis".format(axis))
         time_value_s = abs(steps) / (self.STEP_MODE_VALUE * 
-                                              self.PWM_FREQUENCY_LIST[frequency_index] * self.DISTANCE_PER_STEP)
+                                              self.PWM_FREQUENCY_LIST[frequency_index] * self.STEP_MODE_VALUE)
         self.hal.moveStepperMotor(step_pin, direction_pin, 
                                   direction, self.DUTY_CYCLE_HALF, frequency_index)
-        time.sleep(time_value_s) # there's probably a better way to do this -> update: there's actually a worse way :3 ^
+        time.sleep(time_value_s)
         self.hal.stopStepperMotor(step_pin, direction_pin)
 
     def moveMotor(self, distance_value, axis:str): # tooling to control motor movements by axis
@@ -185,11 +212,13 @@ class Solenoid():
             self.shutdown()
         # time.sleep(time_value_s) # there's probably a better way to do this -> update: there's actually a worse way :3 ^
         self.hal.stopStepperMotor(step_pin, direction_pin)
+
     def homeMotor(self, axis:str): # resets motors to origin
         match axis:
             case 'X':
                 if self.hal.pi.read(self.X_SWITCH_PIN) == 0:
                     print("Already homed")
+                    # moves the motor off the limit switch
                     self.hal.moveStepperMotor(self.LOCOMOTIVE_STEP_PIN_X, self.DIRECTION_PIN_X, 
                                   self.DIRECTION_POSITIVE, self.DUTY_CYCLE_HALF, self.PWM_FREQUENCY_INDEX)
                     time.sleep(0.3)
@@ -201,6 +230,7 @@ class Solenoid():
                 if self.hal.pi.wait_for_edge(self.X_SWITCH_PIN, self.FALLING_EDGE):
                     print("Limit switch event detected")
                     self.hal.stopStepperMotor(self.LOCOMOTIVE_STEP_PIN_X, self.DIRECTION_PIN_X)
+                    # moves the motor off the limit switch
                     self.hal.moveStepperMotor(self.LOCOMOTIVE_STEP_PIN_X, self.DIRECTION_PIN_X, 
                                   self.DIRECTION_POSITIVE, self.DUTY_CYCLE_HALF, self.PWM_FREQUENCY_INDEX)
                     time.sleep(0.3)
@@ -228,23 +258,28 @@ class Solenoid():
                 else:
                     raise TimeoutError("Homing not complete")
             case 'T':
-                # If a turntable is added, uncomment this section
-                # step_pin = self.LOCOMOTIVE_STEP_PIN_T
-                # switch_pin = self.T_SWITCH_PIN
+                # If a turntable is added, add to this section
+                # 'T' corresponds to the theta axis, which would be turning
                 print("Homing T not yet implemented")
             case _:    
                 raise ValueError("Error in homeMotor: {} is an invalid axis".format(axis))
+            
     def pumpOn(self):
         self.hal.moveStepperMotor(self.PERISTALTIC_STEP_PIN, None, 0, self.DUTY_CYCLE_HALF, self.PWM_FREQUENCY_INDEX)
+
     def pumpOff(self):
         self.hal.stopStepperMotor(self.PERISTALTIC_STEP_PIN, self.LOCOMOTIVE_DIRECTION_PIN)
+
     def openAirValve(self):
         self.hal.setPinHigh(self.AIR_VALVE_PIN)
+
     def closeAirValve(self):
         self.hal.setPinLow(self.AIR_VALVE_PIN)
-    def pwmAirValve(self, pwm_value):
+
+    def pwmAirValve(self, pwm_value): #allows air valve to open & close rapidly, maybe don't use this
         self.hal.setPWM(self.AIR_VALVE_PIN, pwm_value, self.VALVE_FREQUENCY_INDEX)
-    def shutdown(self):
+
+    def shutdown(self): #shutdown sequence for emergencies
         print("Shutting down")
         self.closeAirValve()
         self.pumpOff()
@@ -253,11 +288,12 @@ class Solenoid():
         raise SystemError("Shutdown sequence called")
 
 class SCodeParse():
-    # Fluid handling configs
+    # Fluid handling configs, purging is clearing the fluid line, loading is added new solution to the fluid line
     PURGE_TIME = 10 #dummy value
     LOAD_TIME = 10 #dummy value
     X_LENGTH = 240 # length of nozzle carriage track
     Y_LENGTH = 80 # length of available movement area for baseplate
+
     def __init__(self, Solenoid = Solenoid()):
         self.motor_solenoid = Solenoid
         self.command_vector = []
@@ -302,14 +338,14 @@ class SCodeParse():
             state = self.command_vector[line_number][1]
             self.mneumonicMatch(mneumonic, state)
 
-    def checkPathDisplacement(self):
+    def checkPathDisplacement(self): # ensures that the motors don't try to push the carriage or base plate off the track
         print("Executing path displacement check")
         x_displacement = 0
         y_displacement = 0
         for line_number in range(len(self.command_vector)):
             mneumonic = self.command_vector[line_number][0]
             state = self.command_vector[line_number][1]
-            if mneumonic == 'MOVE':
+            if mneumonic == 'MOVE': #only pulls out the commands that move everything
                 try:
                     axis = str(state[0])
                 except:
@@ -332,9 +368,6 @@ class SCodeParse():
         if y_displacement >= self.Y_LENGTH:
             raise ValueError("Nozzle carriage will go out of range, check your path code")
             
-        
-
-
     
     def purgeSequence(self):
         self.motor_solenoid.setReservoirSelect(0) #MAGIC NUMBER ALERT, please fix later
